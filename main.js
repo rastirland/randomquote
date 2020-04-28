3a94cea515484d1cf520cc1a0f3ae445d0637162
rastirland <rastirland@hotmail.com>
2020-04-28 16:19:55 +0100

    fv

--- main.js
+++ main.js
@@ -13,3 +13,4 @@ function genQuote() {
   let quotes = ["Blank", "\"Remember only, you can determine how postive your day goes.\" - James Young", "\"Associate yourself with people of good quality, for it is better to be alone than in bad company.\"- Booker T. Washington", "\"You are never too old to set another goal or to dream a new dream.\"- C.S Lewis", "\"If you can dream it, you can do it.\"- Walt Disney", "\"Never give up, for that is just the place and time that the tide will turn.\"- Harriet Beecher Stowe", "\"I know where I'm going and I know the truth, and I don't have to be what you want me to be. I'm free to be what I want.\"- Muhammad Ali", "\"If you always put limit on everything you do, physical or anything else. It will spread into your work and into your life. There are no limits. There are only plateaus, and you must not stay there, you must go beyond them.\"- Bruce Lee",];
 
 
+// test
